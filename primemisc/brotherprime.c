cc8603e0cb9f7db959207c2735cd4c94945b56df
saoyagi2 <saoyagi2@gmail.com>
2020-03-04 17:57:10 +0900

    変数名を修正

--- primemisc/brotherprime.c
+++ primemisc/brotherprime.c
@@ -3,7 +3,7 @@
 
 int main( int ac, char *av[] )
 {
-    int n, m, d;
+    int n, p, d;
     char buf[1024];
 
     /*  コマンドラインから差を決定する    */
@@ -11,12 +11,12 @@ int main( int ac, char *av[] )
         return( 1 );
     d = strtol( av[1], NULL, 10 );
 
-    m = -1;
+    p = -1;
     while( fgets( buf, 1024, stdin ) ) {
         n = strtol( buf, NULL, 10 );
-        if( m != -1 && n - d == m )
-            printf( "%d %d\n", m, n );
-        m = n;
+        if( p != -1 && n - d == p )
+            printf( "%d %d\n", p, n );
+        p = n;
     }
 
     return( 0 );
--- primemisc/twinprime.c
+++ primemisc/twinprime.c
@@ -3,15 +3,15 @@
 
 int main( int ac, char *av[] )
 {
-    int n, m;
+    int n, p;
     char buf[1024];
 
-    m = -1;
+    p = -1;
     while( fgets( buf, 1024, stdin ) ) {
         n = strtol( buf, NULL, 10 );
-        if( m != -1 && n - 2 == m )
-            printf( "%d %d\n", m, n );
-        m = n;
+        if( p != -1 && n - 2 == p )
+            printf( "%d %d\n", p, n );
+        p = n;
     }
 
     return( 0 );
