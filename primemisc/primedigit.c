7bd288e20d23c219cae3c0cc96ce0b599fa5775c
saoyagi2 <saoyagi2@gmail.com>
2020-03-25 19:45:20 +0900

    primedigit - コードを整理

--- primemisc/primedigit.c
+++ primemisc/primedigit.c
@@ -6,35 +6,26 @@ int countdigit( int n );
 
 int main( int ac, char *av[] )
 {
-    int count, n;
+    int count, i, n, _n, types[10];
     char buf[1024];
 
     while( fgets( buf, 1024, stdin ) ) {
         n = strtol( buf, NULL, 10 );
-        count = countdigit( n );
+
+        for( i = 0; i < 10; i++ )
+            types[i] = 0;
+        _n = n;
+        while( _n != 0 ) {
+		    types[_n % 10]++;
+		    _n /= 10;
+	    }
+        count = 0;
+        for( i = 0; i < 10; i++ )
+            if( types[i] != 0 )
+                count++;
+
         printf( "%d %d\n", n, count );
     }
 
     return( 0 );
-}
-
-
-int countdigit( int n )
-{
-    int     i, types[10], count;
-    char    buf[1024];
-
-    for( i = 0; i < 10; i++ )
-        types[i] = 0;
-
-    sprintf( buf, "%d", n );
-    for( i = 0; i < strlen( buf ); i++ )
-        types[buf[i] - '0']++;
-
-    count = 0;
-    for( i = 0; i < 10; i++ )
-        if( types[i] != 0 )
-            count++;
-
-    return( count );
 }
