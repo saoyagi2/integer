6fed2948be42a2bd5f2be2d02f91ad88dbedb60d
saoyagi2 <saoyagi2@gmail.com>
2020-04-09 13:18:31 +0900

    ARRAY_SIZE -> ARRAY_SIZE_MAX に

--- yuuai/syakou.c
+++ yuuai/syakou.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-#define ARRAY_SIZE   (100000000)
+#define ARRAY_SIZE_MAX   (100000000)
 
-int array[ARRAY_SIZE];
+int array[ARRAY_SIZE_MAX];
 
 int main( int ac, char *av[] )
 {
     int n, n2, n3, i;
 
     /*  配列を初期化する    */
-    for( i = 0; i < ARRAY_SIZE; i++ )
+    for( i = 0; i < ARRAY_SIZE_MAX; i++ )
         array[i] = 0;
 
     /*  約数の和を求める    */
-    for( n = 1; n < ARRAY_SIZE / 2; n++ ) {
-        for( i = 2; i * n < ARRAY_SIZE; i++ )
+    for( n = 1; n < ARRAY_SIZE_MAX / 2; n++ ) {
+        for( i = 2; i * n < ARRAY_SIZE_MAX; i++ )
             array[i * n] += n;
     }
 
     /*  約数の和が互いの数自身になれば社交数である  */
-    for( n = 1; n < ARRAY_SIZE; n++ ) {
+    for( n = 1; n < ARRAY_SIZE_MAX; n++ ) {
         n2 = array[n];
-        if( n2 >= ARRAY_SIZE )
+        if( n2 >= ARRAY_SIZE_MAX )
             continue;
         n3 = array[n2];
-        if( n3 >= ARRAY_SIZE )
+        if( n3 >= ARRAY_SIZE_MAX )
             continue;
         if( n < n2 && n < n3 && array[n3] == n )
             printf( "%d %d %d\n", n, n2, n3 );
--- yuuai/yuuai.c
+++ yuuai/yuuai.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-#define ARRAY_SIZE   (100000000)
+#define ARRAY_SIZE_MAX   (100000000)
 
-int array[ARRAY_SIZE];
+int array[ARRAY_SIZE_MAX];
 
 int main( int ac, char *av[] )
 {
     int n, n2, i;
 
     /*  配列を初期化する    */
-    for( i = 0; i < ARRAY_SIZE; i++ )
+    for( i = 0; i < ARRAY_SIZE_MAX; i++ )
         array[i] = 0;
 
     /*  約数の和を求める    */
-    for( n = 1; n < ARRAY_SIZE / 2; n++ ) {
-        for( i = 2; i * n < ARRAY_SIZE; i++ )
+    for( n = 1; n < ARRAY_SIZE_MAX / 2; n++ ) {
+        for( i = 2; i * n < ARRAY_SIZE_MAX; i++ )
             array[i * n] += n;
     }
 
     /*  約数の和が互いの数自身になれば友愛数である  */
-    for( n = 1; n < ARRAY_SIZE; n++ ) {
+    for( n = 1; n < ARRAY_SIZE_MAX; n++ ) {
         n2 = array[n];
-        if( array[n] < ARRAY_SIZE && n < n2 && array[n2] == n )
+        if( array[n] < ARRAY_SIZE_MAX && n < n2 && array[n2] == n )
             printf( "%d %d\n", n, n2 );
     }
 
