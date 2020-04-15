29ed3d121dbc24c7dfc63fdc0cb80cfe1aeb9297
saoyagi2 <saoyagi2@gmail.com>
2020-04-15 21:17:27 +0900

    ゴールドバッハ予想 - 素数配列を用いるものをgoldbach2、そのGMP版をgmpgoldbach2に分離

---
+++ misc/gmpgoldbach2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+#define ARRAY_SIZE   (100000000)
+#define ARRAY_SIZE_SQRT (10000)
+
+char    array[ARRAY_SIZE];
+
+void initarray( void );
+int isprime( mpz_t n );
+
+int main( int ac, char *av[] )
+{
+    mpz_t n, min_n, max_n, m1, m2;
+
+    /*  コマンドラインから探索範囲を決定する    */
+    if( ac < 3 )
+        return( 1 );
+    mpz_init_set_str( min_n, av[1], 10 );
+    mpz_init_set_str( max_n, av[2], 10 );
+
+    initarray();
+
+    /*  探索範囲の数を調べる    */
+    mpz_init_set( n, min_n );
+    mpz_init( m1 );
+    mpz_init( m2 );
+    while( mpz_cmp( n, max_n ) <= 0 ) {
+        /*  足してnになる整数組(m1,m2)が共に素数か調べる    */
+        mpz_set_ui( m1, 3 );
+        mpz_sub_ui( m2, n, 3 );
+        while( mpz_cmp( m1, m2 ) <= 0 ) {
+            if( isprime( m1 ) && isprime( m2 ) )
+                break;
+            mpz_add_ui( m1, m1, 2 );
+            mpz_sub_ui( m2, m2, 2 );
+        }
+        if( mpz_cmp( m1, m2 ) > 0 )
+            gmp_printf( "%Zd NG\n", n );
+
+        mpz_add_ui( n, n, 2 );
+    }
+
+    mpz_clear( max_n );
+    mpz_clear( n );
+    mpz_clear( m1 );
+    mpz_clear( m2 );
+
+    return( 0 );
+}
+
+void initarray( void )
+{
+    int n, i;
+
+    /*  配列を初期化する    */
+    for( i = 0; i < ARRAY_SIZE; i++ )
+        array[i] = 1;
+
+    /*  配列をふるいにかける    */
+    for( n = 2; n <= ARRAY_SIZE_SQRT; n++ ) {
+        if( array[n] == 1 ) {
+            for( i = n * n; i < ARRAY_SIZE; i += n )
+                array[i] = 0;
+        }
+    }
+}
+
+int isprime( mpz_t n )
+{
+    mpz_t i, n2;
+    int _n, d, ret;
+
+    if( mpz_cmp_ui( n, ARRAY_SIZE ) < 0 ) {
+        _n = mpz_get_ui( n );
+        return( array[_n] );
+    }
+
+    /*  1は素数ではない */
+    if( mpz_cmp_ui( n, 1 ) == 0 )
+        return( 0 );
+
+    /*  2,3は素数 */
+    if( mpz_cmp_ui( n, 2 ) == 0 || mpz_cmp_ui( n, 3 ) == 0 )
+        return( 1 );
+
+    /*  2,3で割り切れたら合成数    */
+    if( mpz_even_p( n ) || mpz_divisible_ui_p( n, 3 ) )
+        return( 0 );
+
+    /*  sqrt(n)を求める */
+    mpz_init( n2 );
+    mpz_sqrt( n2, n );
+
+    /*  n2以下の2,3の倍数以外での剰余が0かどうか調べる   */
+    d = 2;
+    mpz_init_set_ui( i, 5 );
+    ret = 1;
+    while( mpz_cmp( i, n2 ) <= 0 ) {
+        if( mpz_divisible_p( n, i ) ) {
+            ret = 0;
+            break;
+        }
+        mpz_add_ui( i, i, d );
+        d = ( d == 2 ? 4 : 2 );
+    }
+
+    mpz_clear( i );
+    mpz_clear( n2 );
+
+    return( ret );
+}
--- misc/goldbach2.c
+++ misc/goldbach2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <gmp.h>
 
 #define ARRAY_SIZE   (100000000)
 #define ARRAY_SIZE_SQRT (10000)
@@ -8,44 +7,33 @@
 char    array[ARRAY_SIZE];
 
 void initarray( void );
-int isprime( mpz_t n );
+int isprime( int n );
+int isqrt( int x );
 
 int main( int ac, char *av[] )
 {
-    mpz_t n, min_n, max_n, m1, m2;
+    int n, min_n, max_n, m1, m2;
 
     /*  コマンドラインから探索範囲を決定する    */
     if( ac < 3 )
         return( 1 );
-    mpz_init_set_str( min_n, av[1], 10 );
-    mpz_init_set_str( max_n, av[2], 10 );
+    min_n = strtol( av[1], NULL, 10 );
+    min_n = min_n % 2 == 1 ? min_n + 1 : min_n;
+    max_n = strtol( av[2], NULL, 10 );
+    max_n = max_n % 2 == 1 ? max_n + 1 : max_n;
 
     initarray();
 
     /*  探索範囲の数を調べる    */
-    mpz_init_set( n, min_n );
-    mpz_init( m1 );
-    mpz_init( m2 );
-    while( mpz_cmp( n, max_n ) <= 0 ) {
+    for( n = min_n; n <= max_n; n += 2 ) {
         /*  足してnになる整数組(m1,m2)が共に素数か調べる    */
-        mpz_set_ui( m1, 3 );
-        mpz_sub_ui( m2, n, 3 );
-        while( mpz_cmp( m1, m2 ) <= 0 ) {
+        for( m1 = 3, m2 = n - 3; m1 <= m2; m1 += 2, m2 -= 2 ) {
             if( isprime( m1 ) && isprime( m2 ) )
                 break;
-            mpz_add_ui( m1, m1, 2 );
-            mpz_sub_ui( m2, m2, 2 );
         }
-        if( mpz_cmp( m1, m2 ) > 0 )
-            gmp_printf( "%Zd NG\n", n );
-
-        mpz_add_ui( n, n, 2 );
+        if( m1 > m2 )
+            printf( "%d NG\n", n );
     }
-
-    mpz_clear( max_n );
-    mpz_clear( n );
-    mpz_clear( m1 );
-    mpz_clear( m2 );
 
     return( 0 );
 }
@@ -67,47 +55,55 @@ void initarray( void )
     }
 }
 
-int isprime( mpz_t n )
+int isprime( int n )
 {
-    mpz_t i, n2;
-    int _n, d, ret;
+    int i, n2, d;
 
-    if( mpz_cmp_ui( n, ARRAY_SIZE ) < 0 ) {
-        _n = mpz_get_ui( n );
-        return( array[_n] );
+    if( n < ARRAY_SIZE ) {
+        return( array[n] );
     }
 
     /*  1は素数ではない */
-    if( mpz_cmp_ui( n, 1 ) == 0 )
+    if( n == 1 )
         return( 0 );
 
     /*  2,3は素数 */
-    if( mpz_cmp_ui( n, 2 ) == 0 || mpz_cmp_ui( n, 3 ) == 0 )
+    if( n == 2 || n == 3 )
         return( 1 );
 
     /*  2,3で割り切れたら合成数    */
-    if( mpz_even_p( n ) || mpz_divisible_ui_p( n, 3 ) )
+    if( n % 2 == 0 || n % 3 == 0 )
         return( 0 );
 
     /*  sqrt(n)を求める */
-    mpz_init( n2 );
-    mpz_sqrt( n2, n );
+    n2 = isqrt( n );
 
     /*  n2以下の2,3の倍数以外での剰余が0かどうか調べる   */
     d = 2;
-    mpz_init_set_ui( i, 5 );
-    ret = 1;
-    while( mpz_cmp( i, n2 ) <= 0 ) {
-        if( mpz_divisible_p( n, i ) ) {
-            ret = 0;
-            break;
-        }
-        mpz_add_ui( i, i, d );
-        d = ( d == 2 ? 4 : 2 );
+    for( i = 5; i <= n2; i += d, d = ( d == 2 ? 4 : 2 ) ) {
+        if( n % i == 0 )
+            return( 0 );
     }
 
-    mpz_clear( i );
-    mpz_clear( n2 );
+    /*  素数であった    */
+    return( 1 );
+}
 
-    return( ret );
+int isqrt( int x )
+{
+    int s, t;
+
+    if( x == 0 ) return 0;
+    s = 1;
+    t = x;
+    while( s < t ) {
+        s <<= 1;
+        t >>= 1;
+    }
+    do {
+        t = s;
+        s = ( x / s + s ) >> 1;
+    } while( s < t );
+
+    return( t );
 }
