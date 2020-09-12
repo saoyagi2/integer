e4bee386a0e64446a3238478d6a3bbeacbbc4486
saoyagi2 <saoyagi2@gmail.com>
2020-09-12 23:36:21 +0900

    社交数で3個組以上も対象とする

--- yuuai/syakou.c
+++ yuuai/syakou.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 #define ARRAY_SIZE_MAX   (100000000)
+#define LIST_MAX   (100)
 
 int array[ARRAY_SIZE_MAX];
 
 int main( int ac, char *av[] )
 {
-    int n, n2, n3, i;
+    int n, n_list[LIST_MAX], i, j;
 
     /*  配列を初期化する    */
     for( i = 0; i < ARRAY_SIZE_MAX; i++ )
@@ -18,16 +19,24 @@ int main( int ac, char *av[] )
             array[i * n] += n;
     }
 
-    /*  約数の和が互いの数自身になれば社交数である  */
+    /*  約数の和をたどって元の数に戻れば社交数である  */
     for( n = 1; n < ARRAY_SIZE_MAX; n++ ) {
-        n2 = array[n];
-        if( n2 >= ARRAY_SIZE_MAX )
-            continue;
-        n3 = array[n2];
-        if( n3 >= ARRAY_SIZE_MAX )
-            continue;
-        if( n < n2 && n < n3 && array[n3] == n )
-            printf( "%d %d %d\n", n, n2, n3 );
+        n_list[0] = n;
+        for( i = 1; i < LIST_MAX; i++ ) {
+            n_list[i] = array[n_list[i - 1]];
+            if( n_list[i] >= ARRAY_SIZE_MAX || n_list[i] < n ) {
+                break;
+            }
+            if( n_list[i] == n_list[0] ) {
+                if( i > 2 ) {
+                    for( j = 0; j < i; j++ ) {
+                        printf( "%d ", n_list[j] );
+                    }
+                    printf( "\n" );
+                }
+                break;
+            }
+        }
     }
 
     return( 0 );
