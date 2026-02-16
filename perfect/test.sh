59b3ee473a4b6e0cfeae54bea5925566d462bfa7
Shigekazu Aoyagi <saoyagi2@gmail.com>
2026-02-16 14:22:25 +0900

    gmpperfectarray のテスト追加

--- perfect/test.sh
+++ perfect/test.sh
@@ -10,6 +10,10 @@ exectest "./perfectarray 0 0" "bad parameter"
 exectest "./perfectarray 2 10" "6"
 exectest "./perfectarray 2 30" "6 28"
 
+exectest "./gmpperfectarray 0 0" "bad parameter"
+exectest "./gmpperfectarray 2 10" "6"
+exectest "./gmpperfectarray 2 30" "6 28"
+
 exectest "./multiplyperfectarray 0 0" "bad parameter"
 exectest "./multiplyperfectarray 2 10" "1 6"
 exectest "./multiplyperfectarray 2 1000" "1 6 1 28 2 120 1 496 2 672"
