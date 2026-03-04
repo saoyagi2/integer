cd512fcc5d6fff1352d736368b65f3d100f8cd99
Shigekazu Aoyagi <saoyagi2@gmail.com>
2026-03-04 16:02:59 +0900

    約数和配列を繰り返し使用して広範囲の探索を行えるように修正。約数和配列の範囲外は個別計算

--- yuuai/syakouarray.c
+++ yuuai/syakouarray.c
@@ -1,38 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DIVISORSUMLIST_SIZE_MAX   (100000000)
 #define LIST_MAX   (100)
 
 long long divisorsumlist[DIVISORSUMLIST_SIZE_MAX];
 
-int main( void )
+int main( int ac, char *av[] )
 {
-    long long n, n_list[LIST_MAX], i, j;
+    long long divisorsumlistsize, i, j, k, base, n_list[LIST_MAX], min_n, max_n;
 
-    /*  配列を初期化する    */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX; n++ )
-        divisorsumlist[n] = 0;
-
-    /*  約数の和を求める    */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX / 2; n++ ) {
-        for( i = 2; i * n < DIVISORSUMLIST_SIZE_MAX; i++ )
-            divisorsumlist[i * n] += n;
+    /*  コマンドラインから探索範囲を決定する    */
+    if( ac < 3 ) {
+        fprintf( stderr, "usage : yuuai min_n max_n\n" );
+        return( 1 );
+    }
+    min_n = strtoll( av[1], NULL, 10 );
+    max_n = strtoll( av[2], NULL, 10 );
+    if( min_n < 1 || max_n < 1 ) {
+        fprintf( stderr, "bad parameter\n" );
+        return( 1 );
     }
 
-    /*  約数の和をたどって元の数に戻れば社交数である  */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX; n++ ) {
-        n_list[0] = n;
-        for( i = 1; i < LIST_MAX; i++ ) {
-            n_list[i] = divisorsumlist[n_list[i - 1]];
-            if( n_list[i] >= DIVISORSUMLIST_SIZE_MAX || n_list[i] < n )
-                break;
-            if( n_list[i] == n_list[0] ) {
-                if( i > 2 ) {
-                    for( j = 0; j < i; j++ )
-                        printf( "%lld ", n_list[j] );
-                    printf( "\n" );
+    /*  DIVISORSUMLIST_SIZE_MAX分ごとの整数区間を調べる   */
+    for( base = min_n; base <= max_n; base += DIVISORSUMLIST_SIZE_MAX ) {
+        /*  整数区間配列の大きさを決める    */
+        if( max_n - base + 1 > DIVISORSUMLIST_SIZE_MAX )
+            divisorsumlistsize = DIVISORSUMLIST_SIZE_MAX;
+        else
+            divisorsumlistsize = max_n - base + 1;
+
+        /*  配列を初期化する    */
+        for( i = 0; i < divisorsumlistsize; i++ )
+            divisorsumlist[i] = 0;
+
+        /*  約数の和を求める    */
+        for( i = 1; i < ( base + divisorsumlistsize ) / 2; i++ ) {
+            j = ( base + i - 1 ) / i * i;
+            if( j < i * 2 )
+                j = i * 2;
+            for( ; j < base + divisorsumlistsize; j += i )
+                divisorsumlist[j - base] += i;
+        }
+
+        /*  約数の和をたどって元の数に戻れば社交数である  */
+        for( i = 0; i < divisorsumlistsize; i++ ) {
+            n_list[0] = base + i;
+            for( j = 1; j < LIST_MAX; j++ ) {
+                if( n_list[j - 1] < base + divisorsumlistsize ) {
+                    n_list[j] = divisorsumlist[n_list[j - 1] - base];
                 }
-                break;
+                else {
+                    /*  配列の範囲外の場合は個別計算する  */
+                    n_list[j] = 0;
+                    for( k = 1; k <= n_list[j - 1] / 2; k++ ) {
+                        if( n_list[j - 1] % k == 0 )
+                            n_list[j] += k;
+                    }
+                }
+                if( n_list[j] < n_list[0] )
+                    break;
+                if( n_list[j] == n_list[0] ) {
+                    if( j > 2 ) {
+                        for( k = 0; k < j; k++ )
+                            printf( "%lld ", n_list[k] );
+                        printf( "\n" );
+                    }
+                    break;
+                }
             }
         }
     }
--- yuuai/yuuaiarray.c
+++ yuuai/yuuaiarray.c
@@ -1,28 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DIVISORSUMLIST_SIZE_MAX   (100000000)
 
 long long divisorsumlist[DIVISORSUMLIST_SIZE_MAX];
 
-int main( void )
+int main( int ac, char *av[] )
 {
-    long long n, n2, i;
+    long long divisorsumlistsize, i, j, base, n, n2, tmp, min_n, max_n;
 
-    /*  配列を初期化する    */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX; n++ )
-        divisorsumlist[n] = 0;
-
-    /*  約数の和を求める    */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX / 2; n++ ) {
-        for( i = 2; i * n < DIVISORSUMLIST_SIZE_MAX; i++ )
-            divisorsumlist[i * n] += n;
+    /*  コマンドラインから探索範囲を決定する    */
+    if( ac < 3 ) {
+        fprintf( stderr, "usage : yuuai min_n max_n\n" );
+        return( 1 );
+    }
+    min_n = strtoll( av[1], NULL, 10 );
+    max_n = strtoll( av[2], NULL, 10 );
+    if( min_n < 1 || max_n < 1 ) {
+        fprintf( stderr, "bad parameter\n" );
+        return( 1 );
     }
 
-    /*  約数の和が互いの数自身になれば友愛数である  */
-    for( n = 1; n < DIVISORSUMLIST_SIZE_MAX; n++ ) {
-        n2 = divisorsumlist[n];
-        if( n2 < DIVISORSUMLIST_SIZE_MAX && n < n2 && divisorsumlist[n2] == n )
-            printf( "%lld %lld\n", n, n2 );
+    /*  DIVISORSUMLIST_SIZE_MAX分ごとの整数区間を調べる   */
+    for( base = min_n; base <= max_n; base += DIVISORSUMLIST_SIZE_MAX ) {
+        /*  整数区間配列の大きさを決める    */
+        if( max_n - base + 1 > DIVISORSUMLIST_SIZE_MAX )
+            divisorsumlistsize = DIVISORSUMLIST_SIZE_MAX;
+        else
+            divisorsumlistsize = max_n - base + 1;
+
+        /*  配列を初期化する    */
+        for( i = 0; i < divisorsumlistsize; i++ )
+            divisorsumlist[i] = 0;
+
+        /*  約数の和を求める    */
+        for( i = 1; i < ( base + divisorsumlistsize ) / 2; i++ ) {
+            j = ( base + i - 1 ) / i * i;
+            if( j < i * 2 )
+                j = i * 2;
+            for( ; j < base + divisorsumlistsize; j += i )
+                divisorsumlist[j - base] += i;
+        }
+
+        /*  約数の和が互いの数自身になれば友愛数である  */
+        for( i = 0; i < divisorsumlistsize; i++ ) {
+            n = base + i;
+            n2 = divisorsumlist[i];
+            if( n2 < base + divisorsumlistsize ) {
+                if( n < n2 && divisorsumlist[n2 - base] == n )
+                    printf( "%lld %lld\n", n, n2 );
+            }
+            else {
+                /*  配列の範囲外の場合は個別計算する  */
+                tmp = 0;
+                for( j = 1; j <= n2 / 2; j++ ) {
+                    if( n2 % j == 0 )
+                        tmp += j;
+                }
+                if( n == tmp )
+                    printf( "%lld %lld\n", n, n2 );
+            }
+        }
     }
 
     return( 0 );
